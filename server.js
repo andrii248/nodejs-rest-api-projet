a7b9a7b4106909f0e37eb479a724c993d3171bb2
Andrii Krupa <krpaandrii@gmail.com>
2022-10-03 22:16:46 -0400

    DB_HOST moved to .env file which was included in .gitignore

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const modgoose = require("modgoose");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const app = require("./app");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () => console.log("Database connection successful"))
-  }).catch(
+    app.listen(PORT, () => console.log("Database connection successful"));
+  })
+  .catch((error) => {
     console.log(error.message);
-    process.exit(1)
-  );
+    process.exit(1);
+  });
